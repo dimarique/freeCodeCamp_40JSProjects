169bb5e7f0b38005c33c9530ca4ea4665145d773
dimarique <pochtagreen@gmail.com>
2022-08-08 10:12:11 +0300

    reviews created

---
+++ reviews/app.js
@@ -0,0 +1,12 @@
+let text = document.querySelector('.content_text');
+let wrapper = document.querySelector('.content_wrapper');
+const randomColor = () =>
+	Math.floor(10000 + Math.random() * 16777215)
+		.toString(16)
+		.toUpperCase();
+const bttn = document.querySelector('.content_btn');
+bttn.addEventListener('click', function () {
+	let newColor = `#${randomColor()}`;
+	wrapper.style.backgroundColor = newColor;
+	text.textContent = `Background color now is ${newColor}`;
+});
